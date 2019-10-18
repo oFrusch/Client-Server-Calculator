97bc0a0bc44d0e35cc0485ab35daee24ca48e399
unknown <owenc343@gmail.com>
2019-10-18 13:02:15 -0400

    working version of tcp

--- venv/udp_client.py
+++ venv/udp_client.py
@@ -1,20 +1,47 @@
 import argparse
 import socket
 import datetime
+import pickle
 
-def udp_client(port_number: int):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    operand = input("Please enter an operand: ")
-    num1 = input("Please enter your first number: ")
-    num2 = input("Please enter your second number: ")
 
-    equation = num1 + operand + num2
-    equation = equation.encode('ascii')
+def udp_client(port_number: int, filename: str, timeout: float):
+    with open(filename) as file:
+        for line in file:
 
-    client_socket.sendto(equation, ('127.0.0.1', port_number))
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+            # set timeout for non-received packet
+            client_socket.settimeout(timeout)
+
+            values = line.split()
+
+            equation = [values[1], values[0], values[2]]
+
+            # send equation as an array of values for ease of validity checking
+            equation = pickle.dumps(equation)
+
+            client_socket.sendto(equation, ('127.0.0.1', port_number))
+
+            try:
+                answer = client_socket.recv(65535)
+            except:
+                print("Request Timed Out")
+                break
+
+            answer = answer.decode('ascii').split()
+
+            if answer[0] == '200':
+                print(answer[1])
+            elif answer[0] == '300':
+                print('invalid equation submitted')
+
+            client_socket.close()
+
 
 if __name__ == '__main__':
     parse = argparse.ArgumentParser(description="Provide a port number")
     parse.add_argument('-p', metavar='PORT', type=int, default=50000)
+    parse.add_argument('-f', metavar='FILE', type=str, default='equations')
+    parse.add_argument('-t', metavar='TIMEOUT', type=float, default=5.0)
     arguments = parse.parse_args()
-    udp_client(arguments.p)
+    udp_client(arguments.p, arguments.f, arguments.t)
--- venv/udp_server.py
+++ venv/udp_server.py
@@ -1,44 +1,72 @@
 import argparse
 import socket
 import datetime
-import signal
-import sys
+import sys, signal
+import pickle
+
+
+def check_valid_operation(operation: list) -> bool:
+    if not operation:
+        return False
+
+    operators = set()
+    operators.add('-')
+    operators.add('+')
+    operators.add('*')
+    operators.add('/')
+
+    # check for invalid operator
+    if operation[1] not in operators:
+        return False
+
+    # check for division by 0
+    if operation[2] == '0' and operation[1] == '/':
+        return False
+
+    # make sure values are integers
+    try:
+        int(operation[0])
+        int(operation[2])
+    except:
+        return False
+
+    return True
+
 
 def udp_server(port_number: int) -> int:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(('127.0.0.1', port_number))
 
-    operators = ('-', '+', '*', '/')
-
     # run server
     try:
         while True:
             equation, client_addr = server_socket.recvfrom(65535)
-            equation_as_str = equation.decode('ascii')
+
+            # receive equation as a list of strings
+            equation = pickle.loads(equation)
+            equation_as_str = ''.join(equation)
+
             print('Client sent equation ' + equation_as_str)
-            num1 = equation_as_str[0]
-            num2 = equation_as_str[2]
-            operator = equation_as_str[1]
 
-            if operator not in operators:
-                print('invalid operator')
-                continue
-
-            if num2 == '0' and operator == '/':
-                print('can not divide by 0')
+            # invalid operator or division by 0
+            if not check_valid_operation(equation):
+                print('Invalid Equation')
+                ret_ans = "300 -1"
+                server_socket.sendto(ret_ans.encode('ascii'), client_addr)
                 continue
 
             try:
                 print('Answer to equation: ' + str(eval(equation_as_str)))
                 ans = str(eval(equation_as_str))
-                ret_ans = "Status Code: 200 - Answer: " + ans
+                ret_ans = "200 " + ans
                 server_socket.sendto(ret_ans.encode('ascii'), client_addr)
             except:
                 print('Invalid Equation')
-                ret_ans = "Status Code: 300 - Answer: -1"
+                ret_ans = "300 -1"
                 server_socket.sendto(ret_ans.encode('ascii'), client_addr)
+
     except KeyboardInterrupt:
-        print('Exit on Keyboard Interrupt')
+        print('Server Offline')
 
 
 if __name__ == '__main__':
